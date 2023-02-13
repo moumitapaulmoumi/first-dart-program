f80a3d5b05896efe9574e0f7e6765966c98d3a69
moumitapaulmoumi <moumitapaul.moumi@gmail.com>
2023-02-13 14:31:41 +0600

    Initial commit

---
+++ bin/first.dart
@@ -0,0 +1,14 @@
+import 'package:first/first.dart' as first;
+
+void main(List<String> arguments) {
+  //learn comments
+  //print('Hello Dart');
+  print('Hello Dart');
+  //Multi line Comments
+ /* print('I am learning Dart');
+  print('I am learning Dart');
+  print('I am learning Dart');*/
+
+  print('I am learning Dart');
+
+}
